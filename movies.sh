7d6d90718c9922d9342ed33d9efe0a361236ddb5
pi <me@me>
2022-07-18 14:45:52 +0100

    update

--- movies.sh
+++ movies.sh
@@ -1,3 +1,3 @@
 cd /home/pi/.flexget
-xargs -0 -n 1 flexget movie-list add < <(tr \\n \\0 <movies.txt)
+while read in; do flexget movie-list add < <(tr \\n \\0 <movies.txt) "$in"; done < movies.txt
 > movies.txt
