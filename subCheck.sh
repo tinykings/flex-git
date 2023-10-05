b5bc717dbfb88aa0022d43753c395865980307f4
tinykings <92499963+tinykings@users.noreply.github.com>
2023-10-05 07:49:15 -0700

    Delete subCheck.sh

--- subCheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-input_video="$1"
-
-subtitle_index=$(ffprobe -v error -select_streams s -show_entries stream=index:stream_tags=language -of default=nw=1:nk=1 "$input_video" | awk '/^eng$/{print NR-1; exit}')
-ffmpeg -i "$input_video" -map 0:s:"$subtitle_index" -c:s srt output.srt
